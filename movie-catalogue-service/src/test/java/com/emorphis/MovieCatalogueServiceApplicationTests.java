f5585a027443666c6ea86ff297fc724b1fd11800
Ankit Gaikwad <ankitdg321@gmail.com>
2025-02-11 22:23:59 +0530

    3 services added

---
+++ movie-catalogue-service/src/test/java/com/emorphis/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emorphis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
